d960c55417f891ee3a40bfd255783520082a5173
guyt6517 <guyt6516@gmail.com>
2025-11-04 09:48:13 -0600

    Add update and retrieve endpoints for chair data
    Implement endpoints to update and retrieve chair data.

--- app.py
+++ app.py
@@ -3,4 +3,17 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route('/chairupdate', methods = ['POST'])
 def update():
-  data = request.get_json
+  data = request.get_json()[0]
+  with open("data.txt", "w") as f:
+    f.write(data)
+
+@app.route('/chairget', methods = ['GET'])
+def ret():
+  with open("data.txt", "r") as f:
+    data = f.read()
+  return jsonify({
+    "status": data
+  })
+
+if __name__ == '__main__':
+  app.run(debug = True)
